48d8d60dcdc812191ede01299ba502198902bfab
tristen <tristen.brown@gmail.com>
2015-07-02 17:30:33 -0400

    basic routing example

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 'use strict';
 
-var lrm = require('leaflet-routing-machine');
+var Geocoder = require('leaflet-control-geocoder');
+require('leaflet-routing-machine');
 
-console.log('hey there', lrm);
+var options = require('./src/lrm_options');
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+var ReversablePlan = L.Routing.Plan.extend({
+  createGeocoders: function() {
+    var container = L.Routing.Plan.prototype.createGeocoders.call(this)
+    return container;
+  }
+});
+
+var plan = new ReversablePlan([], {
+  geocoder: Geocoder.nominatim(),
+  routeWhileDragging: true,
+  position: 'topright',
+  useZoomParameter: true,
+  reverseWaypoints: true,
+  dragStyles: options.lrm.dragStyles,
+    geocodersClassName: options.lrm.geocodersClassName
+  }),
+  control = L.Routing.control({
+      routeWhileDragging: true,
+      plan: plan,
+      lineOptions: options.lrm.lineOptions,
+  summaryTemplate: options.lrm.summaryTemplate,
+  containerClassName: options.lrm.containerClassName,
+    alternativeClassName: options.lrm.alternativeClassName,
+    stepClassName: options.lrm.stepClassName
+}).addTo(map);
+
+var start = true;
+var end = false;
+
+map.on('click', function(e) {
+	if (start) {
+		end = true;
+		start = false;
+		control.spliceWaypoints(0, 1, e.latlng);
+		//control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+	} else if (end) {
+		//control.spliceWaypoints(0, 1, e.latlng);
+		control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+  }
+});
